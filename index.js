3dab8a0323937cac16b6fd220f184ae91aac3661
bhaskarmelkani <melkani.bhaskar@gmail.com>
2018-05-03 22:56:14 +0200

    Initial Commit'

---
+++ index.js
@@ -0,0 +1,39 @@
+require('./text-polyfill');
+
+const noble = require('noble');
+const bleat = require('bleat').webbluetooth;
+const { MUSE_SERVICE, MuseClient, zipSamples, channelNames,  MuseControlResponse, EEGSample } = require('muse-js');
+const { map, share, tap, takeUntil } = require('rxjs/operators');
+
+async function connect() {
+    console.log('Connect called')
+    let device = await bleat.requestDevice({
+        filters: [{ services: [MUSE_SERVICE] }]
+    });
+    const gatt = await device.gatt.connect();
+    console.log('Connected Device name:', gatt.device.name);
+
+    const client = new MuseClient();
+    await client.connect(gatt);
+    client.controlResponses.subscribe(x => console.log('Response:', x));
+    await client.start();
+    console.log('Connected!');
+
+    const keepaliveTimer = setInterval(() => client.sendCommand(''), 3000);
+
+    client.eegReadings.subscribe(reading => {
+      console.log(reading);
+    });
+    client.telemetryData.subscribe(telemetry => {
+      console.log(telemetry);
+    });
+    client.accelerometerData.subscribe(acceleration => {
+      console.log(acceleration);
+    });
+}
+
+noble.on('stateChange', (state) => {
+    if (state === 'poweredOn') {
+        connect();
+    }
+});
